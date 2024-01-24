1d4cd8a343936fb6bf9252217f7f2666499125f1
jungheejung <heejung.jung.gr@dartmouth.edu>
2024-01-24 10:28:26 -0500

    DOC: update roxygen documentation

--- R/plot_lineplot_onefactor.R
+++ R/plot_lineplot_onefactor.R
@@ -1,30 +1,45 @@
-plot_lineplot_onefactor <- function(data, taskname, iv,  mean, error,
+#' Plot a Line Plot for One Factor
+#'
+#' This function creates a line plot with error bars for a specified factor from the given dataset.
+#' It is designed to plot mean values with associated error margins (like confidence intervals) for a single task.
+#'
+#' @param data A data frame containing the data to plot.
+#' @param iv A string specifying the name of the independent variable column in `data`.
+#' @param mean A string specifying the name of the column in `data` that contains mean values.
+#' @param error A string specifying the name of the column in `data` that contains error margins.
+#' @param color A vector specifying the color(s) to use for the plot lines.
+#' @param xlab A string for the x-axis label.
+#' @param ylab A string for the y-axis label.
+#' @param ggtitle A string for the plot title.
+#'
+#' @return A ggplot object representing the line plot.
+#'
+#' @examples
+#' # Example usage:
+#' # plot_lineplot_onefactor(my_data, "Task 1", "Group", "MeanScore", "ErrorMargin", "blue", "X-Axis", "Y-Axis", "Plot Title")
+#'
+#' @import ggplot2
+#' @export
+plot_lineplot_onefactor <- function(df, iv,  mean, error,
                       color, xlab, ylab, ggtitle) {
-    # iv = "levels_ordered"
-    # mean = mean_per_sub_norm_mean
-    # error = ci
-    subset <- data[which(data$task == taskname), ]
 
-    g <- ggplot(data = subset, aes(
+    g <- ggplot(data = df, aes(
         x = factor(.data[[iv]]),
         y = .data[[mean]]),
-        #group = .data[[iv]],
-        #color = .data[[iv]]),
+
         cex.lab = 1.5, cex.axis = 2, cex.main = 1.5, cex.sub = 1.5) +
         geom_errorbar(aes(
             ymin = (.data[[mean]] - .data[[error]]),
             ymax = (.data[[mean]] + .data[[error]])
         ), width = .1) +
-        geom_line(aes(group = 1), data = subset) +
-        
+        geom_line(aes(group = 1), data = df) +
+
         geom_point() +
-        # scale_x_continuous(breaks = seq(-3, +3, by = 1)) +
-        # scale_y_continuous(breaks = seq(0, 90, by=30), limits=c(0,90)) +
+
         ggtitle(ggtitle) +
         xlab(xlab) +
         ylab(ylab) +
-        # guides(fill=guide_legend(title="Social Endorsement Position")) +
-        #scale_color_manual(values = color) +
+
         theme_classic() +
         theme(legend.position = "none") +
         theme(aspect.ratio = .6)
